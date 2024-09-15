998ad07715fc0d60ac1ccd4aea1557018e0b519b
b0urn3 <154826956+q4n0@users.noreply.github.com>
2024-09-15 19:24:25 +0300

    Update pikazsh.sh

--- pikazsh.sh
+++ pikazsh.sh
@@ -9,11 +9,22 @@ is_zsh_installed() {
     command -v zsh >/dev/null 2>&1
 }
 
+# Function to check if GCC is installed
+is_gcc_installed() {
+    command -v gcc >/dev/null 2>&1
+}
+
 # Function to install ZSH (if not installed)
 install_zsh() {
     if ! is_zsh_installed; then
         echo -n "Installing ZSH..."
-        (sudo apt install zsh -y >/dev/null 2>&1) & spinner
+        (sudo apt update -y >/dev/null 2>&1) && (sudo apt install zsh -y >/dev/null 2>&1) & spinner
+        if [ $? -ne 0 ]; then
+            echo "Failed to install ZSH. Exiting."
+            exit 1
+        fi
+    else
+        echo "ZSH is already installed."
     fi
 }
 
@@ -22,6 +33,12 @@ switch_to_zsh() {
     if [[ "$SHELL" != "/bin/zsh" ]]; then
         echo -n "Switching to ZSH..."
         (chsh -s /bin/zsh "$USER" >/dev/null 2>&1) & spinner
+        if [ $? -ne 0 ]; then
+            echo "Failed to switch to ZSH. Exiting."
+            exit 1
+        fi
+    else
+        echo "ZSH is already the default shell."
     fi
 }
 
@@ -29,25 +46,48 @@ switch_to_zsh() {
 clone_repo() {
     echo -n "Cloning repository..."
     (git clone "$1" "$2" >/dev/null 2>&1) & spinner
+    if [ $? -ne 0 ]; then
+        echo "Failed to clone repository. Exiting."
+        exit 1
+    fi
 }
 
 # Function to compile the C code and rename the executable
 compile_and_rename() {
+    if ! is_gcc_installed; then
+        echo "GCC is not installed. Please install GCC and try again."
+        exit 1
+    fi
     echo -n "Compiling C code..."
     (cd "$1" && gcc -o term terminalpika.c >/dev/null 2>&1) & spinner
+    if [ $? -ne 0 ]; then
+        echo "Failed to compile C code. Exiting."
+        exit 1
+    fi
 }
 
 # Function to move the executable to /usr/bin
 move_executable() {
     echo -n "Moving executable to /usr/bin..."
     (sudo mv "$1/term" "/usr/bin/term" >/dev/null 2>&1) & spinner
+    if [ $? -ne 0 ]; then
+        echo "Failed to move executable. Exiting."
+        exit 1
+    fi
 }
 
 # Function to replace the zshrc file
 replace_zshrc() {
     echo -n "Replacing .zshrc..."
-    [ -f "$HOME/.zshrc" ] && cp "$HOME/.zshrc" "$HOME/.zshrc.bak"
+    if [ -f "$HOME/.zshrc" ]; then
+        echo "Backing up existing .zshrc to .zshrc.bak"
+        cp "$HOME/.zshrc" "$HOME/.zshrc.bak"
+    fi
     (cp "$1/.zshrc" "$HOME/.zshrc" >/dev/null 2>&1) & spinner
+    if [ $? -ne 0 ]; then
+        echo "Failed to replace .zshrc. Exiting."
+        exit 1
+    fi
 }
 
 # Function to delete all related files
