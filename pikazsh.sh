3571581160a241270d217e04952a60ef97b08db9
b0urn3 <154826956+q4n0@users.noreply.github.com>
2024-09-22 15:59:14 +0300

    Update pikazsh.sh

--- pikazsh.sh
+++ pikazsh.sh
@@ -1,81 +1,49 @@
 #!/bin/bash
 
-# Define variables
-GITHUB_REPO="https://github.com/q4n0/terminalpika"
-ZSHRC_URL="$GITHUB_REPO/.zshrc"
-TERMPIKA_URL="$GITHUB_REPO/termpika.c"
-TERM_BIN="/usr/bin/term"
-
-# Function to print banners
-print_banner() {
-    echo -e "\033[31m"  # Red color
-    echo "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠶⠤⢤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-    echo "⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡤⠶⠾⠷⠶⠶⣤⣈⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-    echo "⠀⠀⠀⠀⠀⠀⣠⠾⢁⣤⣴⣶⣶⣶⣶⣦⣤⡉⠛⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-    echo "⠀⠀⠀⠀⢀⡾⢡⠞⠻⣿⣿⡿⢻⣿⣿⣿⣿⡿⠃⠀⠻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-    echo "⠀⠀⠀⠀⣾⢡⣿⠀⠀⣿⡟⠀⠀⣿⣿⣿⣿⣇⠀⠀⡀⢻⣆⣤⠶⠒⠒⠶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-    echo "⠀⠀⠀⠀⣿⣾⣿⣄⣴⣿⣷⡀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣷⠘⣿⠧⣤⠻⠽⠂⢈⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-    echo "⠀⠀⣠⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢸⣿⡛⠛⣴⣲⠄⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-    echo "⢰⡟⠁⠀⠈⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢡⡿⠙⢿⣦⣤⣤⣤⠾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-    echo "⢸⡇⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢛⣵⠟⠀⠀⣼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-    echo "⠈⠻⣆⠀⠀⠀⠀⠀⣻⠾⢿⣿⣛⣛⣻⣯⣵⡾⠛⠁⠀⠀⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-    echo "⠀⠀⠙⠷⣄⣀⣀⣠⡿⠀⠀⠀⠀⠀⠀⠀⠀⢷⡀⠀⠀⢀⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
-    echo "⠀⠀⠀⠀⠈⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⠾⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\033[0m"  # Reset color
-}
-
-# Start script with welcome message
-print_banner
-echo -e "\033[32mWelcome to TerminalPika Setup!\033[0m"  # Green color
-echo -e "\033[33mThis script requires sudo privileges to run successfully.\033[0m"  # Yellow color
+# Welcome Banner
+echo -e "\e[31m⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠶⠤⢤⣀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡤⠶⠾⠷⠶⠶⣤⣈⣷
+⠀⠀⠀⠀⠀⠀⣠⠾⢀⣤⣴⣶⣶⣶⣶⣦⣤⡉⠛⢦⡀
+⠀⠀⠀⠀⢀⡾⢡⠞⠻⣿⣿⡿⢻⣿⣿⣿⣿⡿⠃⠀⠻⣆
+⠀⠀⠀⠀⣾⢡⣿⠀⠀⣿⡟⠀⠀⣿⣿⣿⣿⣇⠀⠀⡀⢻⣆⣤⠶⠒⠒⠶⣤⡀
+⠀⠀⠀⠀⣿⣾⣿⣄⣴⣿⣷⡀⣠⣿⣿⣿⣿⣿⣿⣿⣷⠘⣿⠧⣤⠻⠽⠂⢈⣿
+⠀⠀⣠⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢸⣿⡛⠛⣴⣲⠄⢸⡇
+⢰⡟⠁⠀⠈⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢡⡿⠙⢿⣦⣤⣤⣤⠾⠃
+⢸⡇⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢛⣵⠟⠀⠀⣼⠃⠀⠀⠀⠀
+⠈⠻⣆⠀⠀⠀⠀⠀⣻⠾⢿⣿⣛⣛⣻⣯⣵⡾⠛⠁⠀⠀⢀⣿⠀⠀⠀⠀⠀
+⠀⠀⠙⠷⣄⣀⣀⣠⡿⠀⠀⠀⠀⠀⠀⠀⠀⢷⡀⠀⠀⢀⣾⠃⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⠾⠛⠁\e[0m"
+echo -e "\nWelcome to TerminalPika Setup!"
+echo "This script requires sudo privileges to run successfully."
 echo "Please enter your password if prompted."
 
-# Confirm using sudo
-if [[ $EUID -ne 0 ]]; then
-    echo -e "\033[31mYou need to run this script with sudo! Exiting...\033[0m"  # Red color
-    exit 1
-fi
+# Set URLs for files
+ZSHRC_URL="https://raw.githubusercontent.com/q4n0/terminalpika/main/zshrc"
+TERMPIKA_C_URL="https://raw.githubusercontent.com/q4n0/terminalpika/main/termpika.c"
 
-# Backup existing .zshrc
-if [ -f ~/.zshrc ]; then
-    echo "Backing up existing .zshrc to ~/.zshrc.bak"
-    cp ~/.zshrc ~/.zshrc.bak
-fi
-
-# Download new .zshrc from GitHub
+# Download new .zshrc
 echo "Downloading new .zshrc..."
 curl -o ~/.zshrc $ZSHRC_URL
 
-# Check if the download was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to download .zshrc from GitHub."
-    exit 1
-fi
-
 # Download termpika.c
 echo "Downloading termpika.c..."
-curl -o ~/termpika.c $TERMPIKA_URL
-
-# Check if the download was successful
-if [ $? -ne 0 ]; then
-    echo "Failed to download termpika.c from GitHub."
-    exit 1
-fi
+curl -o termpika.c $TERMPIKA_C_URL
 
 # Compile termpika.c
 echo "Compiling termpika.c..."
-gcc ~/termpika.c -o $TERM_BIN
-
-# Check if compilation was successful
+gcc -o term termpika.c
 if [ $? -ne 0 ]; then
-    echo "Compilation failed."
+    echo "Compilation failed. Please check the errors above."
     exit 1
 fi
 
-# Change permissions to make the binary executable
-chmod +x $TERM_BIN
+# Move the binary to /usr/bin
+echo "Moving term to /usr/bin..."
+mv term /usr/bin/
 
-# Cleanup downloaded files
-rm ~/termpika.c
+# Clean up downloaded files
+echo "Cleaning up..."
+rm termpika.c
 
-echo "Setup completed successfully!"
-echo "You can now run 'term' in your terminal."
+# Completion message
+echo -e "Setup complete! You can now run 'term' in your terminal.\n"
